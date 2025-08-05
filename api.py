f9f51cb41b4273066ebed5fc84e44f4aad3aa850
jhrp157 <Jose-rodriguez@correo.unimet.edu.ve>
2025-08-05 12:40:06 -0400

    obtener departamentos del api

--- api.py
+++ api.py
@@ -1,13 +1,39 @@
 import pip._vendor.requests as requests
 
-url = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=1"
+from Departamento import Departamento
 
-data = requests.get(url)
+def api_departaments():
+    departamentos = []
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
 
-response = data.json()
+    data = requests.get(url)
 
-for k in response:
-    print(k)
+    response = data.json()
+
+    for k in response["departments"]:
+        departamento = Departamento(k["departmentId"], k["displayName"])
+        departamentos.append(departamento)
 
 
+def api_obras():
+    obras = []
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
 
+    data = requests.get(url)
+
+    listado_id = data.json()
+
+    for k in listado_id["objectIDs"]:
+        url2 = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + str(k)
+        #print(url2)
+
+
+api_departaments()
+api_obras()
+
+r="https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=1&q=*&hasImages=true"
+data = requests.get(r)
+
+listado_id = data.json()
+
+print(listado_id)
